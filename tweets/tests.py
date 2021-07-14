d9ffecc0af3665061e4daec9978445e3eb059309
Lloyds01 <soloyede708@gmail.com>
2021-07-14 18:45:28 +0800

    make it better

--- tweets/tests.py
+++ tweets/tests.py
@@ -3,7 +3,7 @@ from django.http import response
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
-from tweets.models import Comment, Like, Tweet
+from tweets.models import Comment, Like, Tweet,Retweet
 
 class TweetTests(APITestCase):
     def test_create_tweet(self):
@@ -21,7 +21,21 @@ class TweetTests(APITestCase):
         self.assertEqual(Tweet.objects.get().text, data['text'])
         self.assertEqual(response_data['text'], data['text'])
 
-class TweetTests(APITestCase):
+    def test_patch_tweet(self):
+        
+        user = User.objects.create_user(username="segun", password="forlan123")
+        tweet =Tweet.objects.create(text="baba eleba",user=user)
+
+        url = reverse('tweet-detail',kwargs={"pk":tweet.id})
+        data = {'text': 'DabApps', "user": user.id}
+        response = self.client.patch(url, data, format='json')
+        response_data = response.json()
+        print()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Tweet.objects.count(), 1)
+        self.assertEqual(Tweet.objects.delete().text, data['text'])
+        self.assertEqual(response_data['text'], data['text'])
+
     def test_put_tweet(self):
         
         user = User.objects.create_user(username="segun", password="forlan123")
@@ -38,7 +52,7 @@ class TweetTests(APITestCase):
         self.assertEqual(Tweet.objects.get().text, data['text'])
         self.assertEqual(response_data['text'], data['text'])
 
-class TweetTests(APITestCase):
+
     def test_patch_tweet(self):
         
         user = User.objects.create_user(username="segun", password="forlan123")
@@ -48,11 +62,11 @@ class TweetTests(APITestCase):
         data = {'text': 'DabApps', "user": user.id}
         response = self.client.patch(url, data, format='json')
         response_data = response.json()
-        print("COMPLETE")
+        print(response_data,"COMPLETE")
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-class TweetTests(APITestCase):
+
     def test_get_tweet(self):
         
         user = User.objects.create_user(username="segun", password="forlan123")
@@ -62,11 +76,11 @@ class TweetTests(APITestCase):
         data = {'text': 'DabApps', "user": user.id}
         response = self.client.get(url, data, format='json')
         response_data = response.json()
-        print("COMPLETE")
+        print(response_data,"COMPLETE")
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-class TweetTests(APITestCase):
+
     def test_delete_tweet(self):
         
         user = User.objects.create_user(username="seun", password="foran123")
@@ -94,40 +108,12 @@ class RetweetTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
 
-class RetweetTests(APITestCase):
-    def test_put_retweet(self):
-        
-        user = User.objects.create_user(username="segun", password="forlan123")
-        tweet = Tweet.objects.create(text="this i a", user=user)
-
-        url = reverse('tweet-detail',kwargs={"pk":tweet.id})
-        data = {'tweet': tweet.id, "user": user.id}
-        response = self.client.put(url, data, format='json')
-        response_data = response.json()
-        print(response_data)
-
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-class RetweetTests(APITestCase):
-    def test_patch_retweet(self):
-        user =User.objects.create_user(username='bala',password='murder12')
-        tweet = Tweet.objects.create(text='melanin poping',user=user)
-
-        url= reverse('tweet-list')
-        data = {'tweet':tweet.id, 'user':user.id}
-        response = self.client.patch(url,data,format='json')
-        response_data=response.json()
-        print(response_data)
-
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
-class RetweetTests(APITestCase):
-     def test_get_retweet(self):
+    def test_get_retweet(self):
 
         user=User.objects.create_user(username='stan',password='wendy23')
         tweet = Tweet.objects.create(text='talking',user=user)
 
-        url= reverse('tweet-list')
+        url= reverse('retweet-list')
         data={'tweet':tweet.id,'user':user.id}
         response= self.client.get(url,data,format='json')
         response_data=response.json()
@@ -135,13 +121,13 @@ class RetweetTests(APITestCase):
 
         self.assertEqual(response.status_code,status.HTTP_200_OK)
 
-class RetweetTests(APITestCase):
-     def test_delete_retweet(self):
+
+    def test_delete_retweet(self):
 
         user = User.objects.create_user(username='bala',password='thunder')
         tweet = Tweet.objects.create(text='testing microphone',user=user)
 
-        url = reverse('tweet-list')
+        url = reverse('retweet-list')
         data ={'tweet':tweet.id,'user':user.id}
         response=self.client.delete(url,data,)
         response_data = response.json()
@@ -149,7 +135,20 @@ class RetweetTests(APITestCase):
 
         self.assertEqual(response.status_code,status.HTTP_405_METHOD_NOT_ALLOWED)
 
+    def test_get_one_retweet(self):
 
+        user=User.objects.create_user(username='tunde',password='tesla')
+        tweet=Tweet.objects.create(text="bingo come", user=user)
+        retweet = Retweet.objects.create(user=user,tweet=tweet)
+
+        url = reverse('retweet-detail',kwargs={'pk':retweet.id})
+        data = {'tweet':tweet.id, 'user':user.id, 'retweet':retweet.id}
+        response = self.client.get(url, data, format='json')
+        response_data = response.json()
+        print(response_data)
+
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        # self.assertEqual(response_data,tweet)
 
 class LikeTests(APITestCase):
     def test_create_like(self):
@@ -165,7 +164,7 @@ class LikeTests(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-class LikeTest(APITestCase):
+
     def test_get_like(self):
 
         """test for getting"""
@@ -179,7 +178,7 @@ class LikeTest(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-class LikeTest(APITestCase):
+
     def test_patch_like(self):
 
         user = User.objects.create_user(username="bola",password="forlan")
@@ -193,21 +192,7 @@ class LikeTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 
-class LikeTest(APITestCase):
-    def test_put_like(self):
-        
-        user = User.objects.create_user(username='brian',password='fox')
-        tweet = Tweet.objects.create(text='py.mongo', user=user)
 
-        url = reverse('tweet-list')
-        data = {'tweet':tweet.id,'user':user.id}
-        response = self.client.put(url,data,format='json')
-        print('done')
-
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
-
-class LikeTest(APITestCase):
     def test_delete_like(self):
 
         user = User.objects.create_user(username='best', password='lean')
@@ -220,89 +205,151 @@ class LikeTest(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
-class CommentTests(APITestCase):
-    def test_create_comment(self):
+    def test_get_one_like(self):
+
         
-        user = User.objects.create_user(username="lloyds", password="forlan123")
-        tweet = Tweet.objects.create(text="this is a comment", user=user)
-
-        url = reverse('comment-list')
-        data = {'tweet': tweet.id, "user": user.id}
-        response = self.client.post(url, data, format='json')
-        response_data = response.json()
-        print(response_data)
-
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        user = User.objects.create_user(username="segun", password="bela")
+        tweet = Tweet.objects.create(text="random",user=user)
+        like = Like.objects.create(tweet=tweet,user=user)
 
 
-class TweetTests(APITestCase):
-    def test_patch_tweet(self):
-        
-        user = User.objects.create_user(username="segun", password="forlan123")
-        tweet =Tweet.objects.create(text="baba eleba",user=user)
-
-        url = reverse('tweet-detail',kwargs={"pk":tweet.id})
-        data = {'text': 'DabApps', "user": user.id}
-        response = self.client.patch(url, data, format='json')
-        response_data = response.json()
-        print()
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        # self.assertEqual(Tweet.objects.count(), 1)
-        # self.assertEqual(Tweet.objects.delete().text, data['text'])
-        # self.assertEqual(response_data['text'], data['text'])
-
-class RetweetTestsP(APITestCase):
-    def test_get_retweet(self):
-        
-        user = User.objects.create_user(username="segun", password="forlan123")
-        tweet = Tweet.objects.create(text="this is a simple tweet", user=user)
-
-        url = reverse('retweet-list')
-        data = {'tweet': tweet.id, "user": user.id}
-        response = self.client.get(url, data, format='json')
-        response_data = response.json()
-        print(response_data)
+        url = reverse("like-detail",kwargs={'pk':like.id})
+        data ={'tweet':tweet.id, "user":user.id,'like':like.id}
+        response = self.client.get(url,data,format='json')
+        response_data =response.json()
+        print("done")
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Like.objects.count(), 1)
+        # self.assertEqual(response_data[0],tweet.id)
 
-class LikeTestsP(APITestCase):
-    def test_get_like(self):
-        
-        user = User.objects.create_user(username="segun", password="forlan123")
-        tweet = Tweet.objects.create(text="created tweet", user=user)
 
-        url = reverse('like-list')
-        data = {'tweet': tweet.id, "user": user.id}
-        response = self.client.get(url, data, format='json')
-        response_data = response.json()
-        print(response_data)
-
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 class CommentTests(APITestCase):
-    def test_get_comment(self):
+     def test_create_comment(self):
         
-        user = User.objects.create_user(username="lloyds", password="forlan123")
-        tweet = Tweet.objects.create(text="this is a comment", user=user)
+        user = User.objects.create_user(username="lloyds",password='tyu5')
+        tweet = Tweet.objects.create(text='likers plate',user=user)
 
         url = reverse('comment-list')
-        data = {'tweet': tweet.id, "user": user.id}
-        response = self.client.get(url, data, format='json')
+        data={'tweet':tweet.id,'user':user.id}
+        response = self.client.post(url, data,format='json')
+        response_data = response.json()
+        print(response_data)
+        self.assertEqual(response.status_code,status.HTTP_201_CREATED)
+
+     def test_patch_comment(self):
+        
+        user = User.objects.create_user(username='brian',password="talkers")
+        tweet = Tweet.objects.create(text = "party animal",user=user)
+        comment =Comment.objects.create(text = "counter", user=user, tweet=tweet)
+
+        url = reverse('comment-detail',kwargs={"pk":comment.id})
+        data= {'tweet':tweet.id,'user':user.id, 'text':comment.id}
+        response = self.client.patch(url,data,)
         response_data = response.json()
         print(response_data)
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-# class CommentTests(APITestCase):
-#      def test_create_comment(self):
-        
-#         user = User.objects.create_user(usernsme='lloyds',password='tyu5')
-#         tweet = Tweet.objects.create(text='likers plate',user=user)
-
-#         url = reverse('tweet-list')
-#         data={'tweet':tweet.id,'user':user.id}
-#         response = self.client.post(url, data,format='json')
-#         response_data = response.json()
-#         print(response_data)
-
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
       
+     def test_get_comment(self):
+
+        user= User.objects.create_user(username='follow', password='real')
+        tweet= Tweet.objects.create(text='indiana girl', user=user)
+        comment = Comment.objects.create(text='igconfig',tweet=tweet,user=user)
+
+        url = reverse('comment-detail',kwargs={'pk':comment.id})
+        response= self.client.get(url)
+        response_data = response.json()
+        print(response_data)
+        
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+
+     def test_put_comment(self):
+        
+        user = User.objects.create_user(username='brian',password="talkers")
+        tweet = Tweet.objects.create(text = "party animal",user=user)
+        comment =Comment.objects.create(text = "counter", user=user, tweet=tweet)
+
+        url = reverse('comment-detail',kwargs={"pk":comment.id})
+        data= {'tweet':tweet.id,'user':user.id, 'text':comment.id}
+        response = self.client.put(url,data,)
+        response_data = response.json()
+        print(response_data)
+
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+
+     def test_delete_comment(self):
+        
+        user =User.objects.create_user(username='band', password= 'maine')
+        tweet = Tweet.objects.create(text = 'complain args', user=user)
+        comment = Comment.objects.create(text ="bakers are loving",tweet=tweet,user=user)
+
+        url = reverse('comment-detail', kwargs={'pk':comment})
+        data= {'tweet':tweet.id,'user':user.id, 'text':comment.id}
+        response = self.client.delete(url,data)
+        response_data = response.json()
+        print(response_data, "love and light")
+
+        self.assertEqual(response.status_code,status.HTTP_404_NOT_FOUND)
+
+
+     def test_get_one_comment(self):
+
+        user= User.objects.create_user(username='follow', password='real')
+        tweet= Tweet.objects.create(text='indiana girl', user=user)
+        comment = Comment.objects.create(text='igconfig',tweet=tweet,user=user)
+
+        url = reverse('comment-detail',kwargs={'pk':comment.id})
+        data= {'tweet':tweet.id,'user':user.id, 'text':comment.id}
+        response= self.client.get(url,data)
+        response_data = response.json()
+        print(response_data)
+        
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        self.assertEqual(Comment.objects.count(), 1)
+        # self.assertEqual(Comment.objects.get().text, data['text'])
+        # self.assertEqual(response_data[0]['text'],comment.text)
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""# # # class TweetTests(APITestCase):
+# # #     
+
+# # # class RetweetTestsP(APITestCase):
+# # #     def test_get_retweet(self):
+        
+# # #         user = User.objects.create_user(username="segun", password="forlan123")
+# # #         tweet = Tweet.objects.create(text="this is a simple tweet", user=user)
+
+# # #         url = reverse('retweet-list')
+# # #         data = {'tweet': tweet.id, "user": user.id}
+# # #         response = self.client.get(url, data, format='json')
+# # #         response_data = response.json()
+# # #         print(response_data)
+
+# # #         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+# # # class LikeTestsP(APITestCase):
+# # #     def test_get_like(self):
+        
+# # #         user = User.objects.create_user(username="segun", password="forlan123")
+# # #         tweet = Tweet.objects.create(text="created tweet", user=user)
+
+# # #         url = reverse('like-list')
+# # #         data = {'tweet': tweet.id, "user": user.id}
+# # #         response = self.client.get(url, data, format='json')
+# # #         response_data = response.json()
+# # #         print(response_data)
+
+# # #         self.assertEqual(response.status_code, status.HTTP_200_OK)"""
